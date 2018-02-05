f6ec61cc523d0fb0f59c5fc13bff189eed28aff4
Rahul Bothra <f2016015@pilani.bits-pilani.ac.in>
2018-02-05 13:08:16 +0530

    Fix: Port to GTK+3, Sugargame v1.2, Sugar Toolkit3 (#4)
    * Set PEP8 code style for the entire repository
    
    * Fix: Port to GTK3 and Sugargame v1.2
    
    This commit ports the activity to GTK3
    
    modified:   Countries.py
    modified:   activity.py
    modified:   setup.py
    modified:   sugargame/__init__.py
    modified:   sugargame/canvas.py
    modified:   sugargame/event.py

--- map1.py
+++ map1.py
@@ -1,66 +1,89 @@
-#map1.py
-import utils,g,os
+# map1.py
+import utils
+import g
+import os
+
 
 class Map1:
     def __init__(self):
-        self.img=None; self.capital=''; self.xy=None
-        self.side=None # calculated after image loaded
-        self.origin=(g.sx(16),g.sy(8))
-        self.textxy=(g.sx(16),g.sy(17))
+        self.img = None
+        self.capital = ''
+        self.xy = None
+        self.side = None  # calculated after image loaded
+        self.origin = (g.sx(16), g.sy(8))
+        self.textxy = (g.sx(16), g.sy(17))
 
-    def setup(self,country):
-        self.country=country
-        if self.img==None:
-            self.img=utils.load_image('map.png')
-            self.side=float(self.img.get_width())/360.0
-            self.circle=utils.load_image('circle.png',True)
-        ln=len(country); capital=''; lat=''; lon=''; self.xy=None
-        self.latlon=None
-        fname=os.path.join('data','latlon.txt')
-        f=open(fname, 'r')
+    def setup(self, country):
+        self.country = country
+        if self.img is None:
+            self.img = utils.load_image('map.png')
+            self.side = float(self.img.get_width()) / 360.0
+            self.circle = utils.load_image('circle.png', True)
+        ln = len(country)
+        capital = ''
+        lat = ''
+        lon = ''
+        self.xy = None
+        self.latlon = None
+        fname = os.path.join('data', 'latlon.txt')
+        f = open(fname, 'r')
         for line in f.readlines():
-            if line[:ln]==country:
-                c,self.capital,lat,lon=line.rstrip().split(','); break
-        if lat!='':
-            self.latlon='Latitude: '+lat_deg(lat)
-            self.latlon+='  Longitude: '+lon_deg(lon)
-            lat=float(lat); lon=float(lon); x0,y0=self.origin
-            x=lon*self.side+x0; x=int(x)
-            y=y0-lat*self.side; y=int(y)
-            self.xy=(x,y)
+            if line[:ln] == country:
+                c, self.capital, lat, lon = line.rstrip().split(',')
+                break
+        if lat != '':
+            self.latlon = 'Latitude: ' + lat_deg(lat)
+            self.latlon += '  Longitude: ' + lon_deg(lon)
+            lat = float(lat)
+            lon = float(lon)
+            x0, y0 = self.origin
+            x = lon * self.side + x0
+            x = int(x)
+            y = y0 - lat * self.side
+            y = int(y)
+            self.xy = (x, y)
 
     def draw(self):
         g.screen.fill(utils.BLACK)
-        g.screen.blit(self.img,(g.sx(0),0))
-        if self.xy!=None: utils.centre_blit(g.screen,self.circle,self.xy)
-        s=self.country
-        utils.text_blit(g.screen,s,g.font2,self.textxy,utils.CREAM,False)
-        x,y=self.textxy
-        if self.capital!='':
-            s='Capital: '+self.capital
-            utils.text_blit(g.screen,s,g.font1,(x,y+g.sy(1.1)),utils.CREAM,False)
-        if self.latlon!=None:
-            s=self.latlon
-            utils.text_blit(g.screen,s,g.font1,(x,y+g.sy(2)),utils.CREAM,False)
-        
+        g.screen.blit(self.img, (g.sx(0), 0))
+        if self.xy is not None:
+            utils.centre_blit(g.screen, self.circle, self.xy)
+        s = self.country
+        utils.text_blit(g.screen, s, g.font2, self.textxy, utils.CREAM, False)
+        x, y = self.textxy
+        if self.capital != '':
+            s = 'Capital: ' + self.capital
+            utils.text_blit(
+                g.screen, s, g.font1, (x, y + g.sy(1.1)), utils.CREAM, False)
+        if self.latlon is not None:
+            s = self.latlon
+            utils.text_blit(
+                g.screen, s, g.font1, (x, y + g.sy(2)), utils.CREAM, False)
+
+
 def lat_deg(lat0):
-    lat=abs(float(lat0))
-    d=int(lat); m=int((lat-d)*60+.5)
-    l='N'; z=''
-    if m<10: z='0'
-    if float(lat0)<0: l='S'
-    s=str(d)+chr(176)+z+str(m)+"'"+l
+    lat = abs(float(lat0))
+    d = int(lat)
+    m = int((lat - d) * 60 + .5)
+    l = 'N'
+    z = ''
+    if m < 10:
+        z = '0'
+    if float(lat0) < 0:
+        l = 'S'
+    s = str(d) + chr(176) + z + str(m) + "'" + l
     return s
+
 
 def lon_deg(lon0):
-    lon=abs(float(lon0))
-    d=int(lon); m=int((lon-d)*60+.5)
-    l='E'; z=''
-    if float(lon0)<0: l='W'
-    if m<10: z='0'
-    s=str(d)+chr(176)+z+str(m)+"'"+l
+    lon = abs(float(lon0))
+    d = int(lon)
+    m = int((lon - d) * 60 + .5)
+    l = 'E'
+    z = ''
+    if float(lon0) < 0:
+        l = 'W'
+    if m < 10:
+        z = '0'
+    s = str(d) + chr(176) + z + str(m) + "'" + l
     return s
-
-
-
-    
--- pages.py
+++ pages.py
@@ -1,52 +1,66 @@
 # pages.py
-import utils,g,buttons
+import utils
+import g
+import buttons
+
 
 class Pages:
     def __init__(self):
-        self.current=None; self.img=None; self.xy=(g.sx(0),g.sy(.1))
-        self.textxy=(g.sx(1),g.sy(20))
+        self.current = None
+        self.img = None
+        self.xy = (g.sx(0), g.sy(.1))
+        self.textxy = (g.sx(1), g.sy(20))
 
     def draw(self):
-        g.screen.fill((100,0,100))
-        if self.current==None: self.current=1
-        if self.img==None:
-            file1=str(self.current)+'.png'
-            self.img=utils.load_image(file1,False,'pages')
-        g.screen.blit(self.img,self.xy)
-        buttons.on(('fd','bk'))
-        if self.current==1: buttons.off('bk')
-        if self.current==10: buttons.off('fd')
-        s='The Flags of the World page '+str(self.current)
-        utils.text_blit1(g.screen,s,g.font1,self.textxy,utils.CREAM,False)
+        g.screen.fill((100, 0, 100))
+        if self.current is None:
+            self.current = 1
+        if self.img is None:
+            file1 = str(self.current) + '.png'
+            self.img = utils.load_image(file1, False, 'pages')
+        g.screen.blit(self.img, self.xy)
+        buttons.on(('fd', 'bk'))
+        if self.current == 1:
+            buttons.off('bk')
+        if self.current == 10:
+            buttons.off('fd')
+        s = 'The Flags of the World page ' + str(self.current)
+        utils.text_blit1(g.screen, s, g.font1, self.textxy, utils.CREAM, False)
 
     def fd(self):
-        self.current+=1
-        self.img=None
+        self.current += 1
+        self.img = None
 
     def bk(self):
-        self.current-=1
-        self.img=None
+        self.current -= 1
+        self.img = None
 
     def which(self):
-        page=1; ind=0
-        while page<self.current:
-            nr=4
-            if page==6: nr=3
+        page = 1
+        ind = 0
+        while page < self.current:
+            nr = 4
+            if page == 6:
+                nr = 3
             for r in range(nr):
-                for c in range(6): ind+=1
-            page+=1
-        y=0; dx=g.sy(32)/6; dy=g.sy(18.5)/4; nr=4
-        if page==6: nr=3
+                for c in range(6):
+                    ind += 1
+            page += 1
+        y = 0
+        dx = g.sy(32) / 6
+        dy = g.sy(18.5) / 4
+        nr = 4
+        if page == 6:
+            nr = 3
         for r in range(nr):
-            x=g.sx(0)
+            x = g.sx(0)
             for c in range(6):
-                if utils.mouse_in(x,y,x+dx,y+dy):
-                    country=g.countries[ind][0]
+                if utils.mouse_in(x, y, x + dx, y + dy):
+                    country = g.countries[ind][0]
                     return country
-                x+=dx; ind+=1
-                if ind==len(g.countries): return None
-            y+=dy
+                x += dx
+                ind += 1
+                if ind == len(g.countries):
+                    return None
+            y += dy
         return None
-        
-
-        
